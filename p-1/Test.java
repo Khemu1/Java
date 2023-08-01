e69771b34131dac3f192dfe55c9387bbf0c9eca7
Khemu1 <126963532+Khemu1@users.noreply.github.com>
2023-08-01 10:44:33 +0200

    basic

---
+++ p-1/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student obj = new Student();
+        int number = obj.number=12;
+        String name= obj.name="John";
+        System.out.println(name+" "+number);
+    }
+}
