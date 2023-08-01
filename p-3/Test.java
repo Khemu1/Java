3e08867b24b1bab838baa3e3c48dfb77f5c60a77
Khemu1 <126963532+Khemu1@users.noreply.github.com>
2023-08-01 11:01:18 +0200

    basic

---
+++ p-3/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    static int n=5;
+
+    public static void main(String[] args) {
+            int i=0;
+            for (n=n;n>=0;n--)  {
+                i++;
+            }
+        System.out.println(i);
+         }
+
+    }
