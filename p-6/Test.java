795b8075264d0434186951f09140a1dde3643a30
Khemu ☥ <126963532+Khemu1@users.noreply.github.com>
2023-08-03 20:05:27 +0200

    Update Test.java

--- p-6/Test.java
+++ p-6/Test.java
@@ -5,7 +5,7 @@ public class Test {
         Person obj = new Person("George Hany",20,"dressmaking",10000,"Man United");
         Person obj1 = new Person("Mariam Hany",19,"Programmer",20000,"Fayoum");
         Person obj2 = new Person("Mariam Saif",23,"Ai Engineer",35000,"Egypt");
-        obj2.printAllinf();
+        System.out.println(obj2.salaryAfterTaxes(35000));;
     }
 
 }
